28b0bc67c4c1a8ad5ade7fa25716535efd9402f5
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-08-22 19:30:36 +0500

    add: Added initial logic for comment and IntersectionObserver rendering

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,97 @@
 const URL = "https://dummyjson.com/comments"
-const contentBlock = document.querySelector('.content')
-const comment = document.querySelector('.comment')
-const commentText = document.querySelector('.comment__text')
-const commentUser = document.querySelector('.comment__user')
 
 async function getData(url) {
     const data = await fetch(url).then(response => response.json())
-    
-    commentText.innerText = data.comments[0].body
-    commentUser.innerText = data.comments[0].user.username
-    console.log(data)
+    return data
 }
 
-getData(URL)
+async function app(url) {
+    const commentsData = await getData(url)
+    const visibleRows = 10
+    let currentPage = 1
+    let i = 0
+    
+    function displayList(arrayData, rowPerPage, page) {
+        const contentBlock = document.querySelector('.content')
+        contentBlock.innerHTML = ''
+
+        page--
+        const startSlicePos = rowPerPage * page
+        const endSlicePos = startSlicePos + rowPerPage
+
+        const slicedArrayData = arrayData.slice(startSlicePos, endSlicePos)
+
+        slicedArrayData.forEach(element => {
+            const list = document.createElement('ul')
+            const listItemText = document.createElement('li')
+            const listItemUsername = document.createElement('li')
+
+            list.classList.add('comment')
+            listItemText.classList.add('comment__text')
+            listItemUsername.classList.add('comment__user')
+
+            listItemText.innerText = element.body
+            listItemUsername.innerText = element.user.username
+
+            list.appendChild(listItemText)
+            list.appendChild(listItemUsername)
+            contentBlock.appendChild(list)
+        })
+
+        let count = document.querySelector('.info span')
+        let targets = document.querySelectorAll('.comment')
+        let options = {
+            threshold: 1,
+        }
+        let observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    count.innerText = ++i
+                    observer.unobserve(entry.target)
+                }
+            })
+        }, options)
+        if(targets) {
+            targets.forEach(element => {
+                observer.observe(element)
+            })
+        }
+    }
+
+    function displayPagination(arrayData, rowPerPage) {
+        const paginationBlock = document.querySelector('.pagination')
+        const pagesCount = Math.ceil(arrayData.length / rowPerPage)
+
+        for(let i = 0; i < pagesCount; i++) {
+            const paginationCell = displayPaginationButton(i + 1)
+            paginationBlock.appendChild(paginationCell)
+        }
+    }
+
+    function displayPaginationButton(pageCount) {
+        const paginationCell = document.createElement('li')
+        paginationCell.classList.add('pagination__cell')
+        paginationCell.innerText = pageCount
+        
+        if(currentPage === pageCount) {
+            paginationCell.classList.add('active')
+        }
+
+        paginationCell.addEventListener('click', () => {
+            let currentPaginationCell = document.querySelector('li.active')
+            currentPaginationCell.classList.remove('active')
+            paginationCell.classList.add('active')
+
+            currentPage = pageCount
+            displayList(commentsData.comments, visibleRows, currentPage)
+            window.scrollTo(0, 0)
+        })
+
+        return paginationCell
+    }
+
+    displayList(commentsData.comments, visibleRows, currentPage)
+    displayPagination(commentsData.comments, visibleRows)
+}
+
+app(URL)
